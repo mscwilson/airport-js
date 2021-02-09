10489c9c2db5fe61fd3921bc86e762d3863e22b2
PetarSimonovic <petar@simonovic.com>
2021-02-09 15:52:12 +0000

    it allows a plane to take off

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -4,11 +4,18 @@ describe("Airport", function() {
     let airport = new Airport();
     expect(airport.hanger).toEqual([]);
   });
-  
+
   it("can add item to hanger array", function() {
     let airport = new Airport();
     airport.landPlane("plane");
     expect(airport.hanger).toEqual(["plane"]);
+  });
+
+  it("can remove an item from the hanger array", function () {
+    let airport = new Airport();
+    airport.landPlane("plane");
+    airport.takeoffPlane("plane");
+    expect(airport.hanger).not.toContain("plane");
   });
 
 
--- src/Airport.js
+++ src/Airport.js
@@ -2,9 +2,14 @@ class Airport {
   constructor() {
     this.hanger = []
   }
-  
+
 
   landPlane(plane) {
     this.hanger.push(plane);
+  };
+
+  takeoffPlane(plane) {
+    let index = this.hanger.indexOf(plane)
+    this.hanger.splice(index);
   };
 }
